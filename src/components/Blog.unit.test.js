4b649fb0743aa5052440a855e29bc06bb6ff9787
herdeybayor <shereefdeen4real@gmail.com>
2022-06-14 23:35:28 +0100

    Exercise 5.14 done

--- src/components/Blog.unit.test.js
+++ src/components/Blog.unit.test.js
@@ -1,25 +1,47 @@
-import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
 import Blog from "./Blog";
 
-test("displaying a blog render title, but not author, url and likes", () => {
+describe("displaying a blog", () => {
+  let container;
   const blog = {
     title: "This is a new blog",
     author: "John Wick",
     url: "https://google.com",
     likes: 20,
+    user: {
+      username: "test_user",
+    },
+  };
+  const user = {
+    username: "test_user",
   };
 
-  const { container } = render(<Blog blog={blog} />);
+  beforeEach(() => {
+    container = render(<Blog blog={blog} user={user} />).container;
+  });
 
-  const title = container.querySelector("#blog-title");
-  const author = container.querySelector("#blog-author");
-  const url = container.querySelector("#blog-url");
-  const likes = container.querySelector("#blog-likes");
+  test("render title, but not author, url and likes by default", () => {
+    const author = container.querySelector("#blog-author");
+    const url = container.querySelector("#blog-url");
+    const likes = container.querySelector("#blog-likes");
 
-  expect(title).toHaveTextContent("This is a new blog");
-  expect(author).toBeDefined();
-  expect(url).toBeNull();
-  expect(likes).toBeNull();
+    screen.getByText("This is a new blog");
+    expect(author).toBeDefined();
+    expect(url).toBeNull();
+    expect(likes).toBeNull();
+  });
+
+  test("shows author, url and like on view button clicked", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+
+    screen.getByText("This is a new blog");
+    screen.getByText("John Wick");
+    screen.getByText("https://google.com");
+    screen.getByText("20", { exact: false });
+  });
 });
